d32f4c42ea24a00231973e4e8104a65d0c5bc0df
Miranda12m <synysterteensier@outlook.es>
2023-10-11 21:50:39 -0600

    TrenAlSur

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const apiClient = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
   params: {
-    api_key: '2f482695fd390b2d1e6e8e5c0e34e037', 
+    api_key: '', 
   },
 });
 
